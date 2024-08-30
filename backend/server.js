4b58e5158269ab59db3edaccea9a4cb8aaec2347
RAHULDAS6009 <das206053@gmail.com>
2024-08-30 22:09:57 +0530

    connection code added

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 const express=require("express");
-const dotenv=require("dotenv")
+const dotenv=require("dotenv");
+const connectDB = require("./config/db");
+// const { connectDB } = require("./config/db");
 const app=express();
 dotenv.config();
+connectDB()
 app.get("/",(req,res)=>{
     console.log("Hello world")
     res.send("hell")
